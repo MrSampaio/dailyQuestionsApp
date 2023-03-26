b880910c73089266ae814df8cab410b11c54252b
Julio Sampaio <jusampa2@gmail.com>
2023-03-26 18:54:49 -0300

    questionsApp

---
+++ index.js
@@ -0,0 +1,50 @@
+const questions = [
+    "Qual foi a melhor parte do seu dia?",
+    "O que te aborreceu?",
+    "Poderia ter feito algo melhor?",
+    "Qual a sua meta para amanhã?"
+]
+
+const ask = (index = 0) =>{
+    process.stdout.write("\n\n" + questions[index] + " > ")
+}
+
+
+const answers = []
+
+
+process.stdin.on("data", data =>{
+
+    answers.push(data.toString().trim())
+
+    if(answers.length < questions.length){
+        ask(answers.length)
+    } 
+    else {
+        process.exit()
+    }
+    
+    
+}) 
+
+process.on("exit", () =>{
+    console.log(
+        `
+        Show!
+
+        A melhor parte do seu dia foi: ${answers[0]}
+
+        O que te aborreceu foi: ${answers[1]}
+
+        Você poderia ter feito de melhor: ${answers[2]}
+
+        Sua meta para amanhã é: ${answers[3]}
+
+        Volte amanhã para refletir sobre seu dia novamente!
+        `
+    )
+})
+
+ask()
+
+
